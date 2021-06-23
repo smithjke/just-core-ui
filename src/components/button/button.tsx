405c9fa0dd066ed266ca5fccb2496ccfa98e031a
smithjke <smithjke@gmail.com>
2021-06-24 02:06:00 +0300

    v0.0.16

---
+++ src/components/button/button.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { createUseStyles } from 'react-jss';
+import { ThemeConfigContext } from '../common/theme-config-context';
+
+export type ButtonProps = {
+    children?: React.ReactNode;
+    kek?: string;
+    label: string;
+    primary?: boolean;
+};
+
+const useStyles = createUseStyles({
+    Button: {
+        backgroundColor: 'gold',
+    },
+});
+
+export function Button(props: ButtonProps): JSX.Element {
+    const theme = useContext(ThemeConfigContext);
+    const styles = useStyles();
+
+    return (
+        <div className={styles.Button} style={{ border: `10px dashed ${theme.color[theme.param['BUTTON_PRIMARY_COLOR']]}` }}>
+            BTN: ({props.label}) {props.children} :{props.kek}
+        </div>
+    );
+}
